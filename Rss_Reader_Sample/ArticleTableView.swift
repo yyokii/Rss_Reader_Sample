c313ab02a1ee5d10ac2ff90ac2a80a5e730b3e17
yyokii <yyokii.h@gmil.com>
2017-09-24 03:18:53 +0900

    ArticleTableViewの作成

---
+++ Rss_Reader_Sample/ArticleTableView.swift
@@ -0,0 +1,61 @@
+//
+//  ArticleTableView.swift
+//  Rss_Reader_Sample
+//
+//  Created by Yoki on 2017/09/24.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
+
+    
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: style)
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        self.register(UINib(nibName: "ArticleTopTableViewCell", bundle:nil), forCellReuseIdentifier: "ArticleTopTableViewCell")
+        
+        self.register(UINib(nibName: "ArticleTableViewCell", bundle:nil), forCellReuseIdentifier: "ArticleTableViewCell")
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0{
+            return 1
+        } else {
+            return 10
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTopTableViewCell", for: indexPath) as! ArticleTopTableViewCell
+            
+            return cell
+            
+        }else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as! ArticleTableViewCell
+            
+            return cell
+        }
+    }
+    
+}
