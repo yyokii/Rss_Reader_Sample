0d6c4263a7e9c3dca243edddb622ea258a76b5bd
yyokii <yyokii.h@gmil.com>
2017-09-24 14:17:05 +0900

    定数クラスの追加と2,3ページ目の追加

--- Rss_Reader_Sample/ArticleViewController.swift
+++ Rss_Reader_Sample/ArticleViewController.swift
@@ -22,7 +22,9 @@ class ArticleViewController: UIViewController {
         self.scrollView.contentSize = CGSize(width: self.view.frame.width * 3, height: self.view.frame.height)
         self.scrollView.isPagingEnabled = true
         
-        setArticleTableView(0, siteName: "classmethod", siteImageName: "classmethodImage", siteURL: "http://dev.classmethod.jp/category/smartphone/feed/")
+        setArticleTableView(0, siteName: Constant.classmethod, siteImageName: Constant.classmethodImageName, siteURL: Constant.classmethodURL)
+        setArticleTableView(self.view.frame.width, siteName: Constant.lifehacker, siteImageName: Constant.lifehackerImageName, siteURL: Constant.lifehackerURL)
+        setArticleTableView(self.view.frame.width*2, siteName: Constant.wired, siteImageName: Constant.wiredImageName, siteURL: Constant.wiredURL)
     }
     
     func setArticleTableView (_ x: CGFloat, siteName: String, siteImageName: String, siteURL: String){
---
+++ Rss_Reader_Sample/Constant.swift
@@ -0,0 +1,25 @@
+//
+//  Constant.swift
+//  Rss_Reader_Sample
+//
+//  Created by Yoki on 2017/09/24.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import Foundation
+
+final class Constant {
+    
+    static let classmethodURL = "http://dev.classmethod.jp/category/smartphone/feed/"
+    static let lifehackerURL = "https://www.lifehacker.jp/feed/index.xml"
+    static let wiredURL = "https://wired.jp/rssfeeder/"
+    
+    static let classmethod = "classmethod"
+    static let lifehacker = "lifehacker"
+    static let wired = "wired"
+    
+    static let classmethodImageName = "classmethodImage"
+    static let lifehackerImageName = "lifehackerImage"
+    static let wiredImageName = "wiredImage"
+    
+}
